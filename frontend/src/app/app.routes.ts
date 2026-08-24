6b6e86d51b562d3873a58b611bf0a5d97ee25433
Christian Murua <cristianmurua.18@gmail.com>
2026-08-24 07:39:19 -0300

    feature: creo componente dashboard admin y agrego ruta

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { SobreNosotros } from './pages/sobre-nosotros/sobre-nosotros';
 import { Pagina404 } from './pages/pagina-404/pagina-404';
+import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
 
 export const routes: Routes = [
   { path: 'sobre-nosotros', component: SobreNosotros },
+  { path: 'dashboard-admin', component: DashboardAdmin },
   { path: '**', component: Pagina404 },
 ];
