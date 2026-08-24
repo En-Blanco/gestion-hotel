3214908a824406dbd796568384b1612b4b625d1e
MarianoBattista <battista.mariano.i@gmail.com>
2026-08-24 12:17:03 -0300

    feat: migracion de landign page a Angular

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { SobreNosotros } from './pages/sobre-nosotros/sobre-nosotros';
 import { Pagina404 } from './pages/pagina-404/pagina-404';
 import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
+import { LandingPage } from './pages/landing-page/landing-page'; 
 
 export const routes: Routes = [
   { path: 'sobre-nosotros', component: SobreNosotros },
   { path: 'dashboard-admin', component: DashboardAdmin },
+  { path: '', component: LandingPage },
   { path: '**', component: Pagina404 },
 ];
