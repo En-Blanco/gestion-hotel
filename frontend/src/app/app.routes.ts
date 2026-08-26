2e3364116a9e0551ef7f18b2f78f446bca5ee747
Marysol Ranieri <lar_sol22@live.com>
2026-08-26 14:22:19 -0300

    feat: adaptar login y agregar ruta en Angular

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { SobreNosotros } from './pages/sobre-nosotros/sobre-nosotros';
 import { Pagina404 } from './pages/pagina-404/pagina-404';
 import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
 import { LandingPage } from './pages/landing-page/landing-page'; 
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
   { path: 'sobre-nosotros', component: SobreNosotros },
   { path: 'dashboard-admin', component: DashboardAdmin },
+  { path: 'login', component: Login },
   { path: '', component: LandingPage },
   { path: '**', component: Pagina404 },
 ];
