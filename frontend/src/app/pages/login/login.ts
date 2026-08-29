74f9a59e511a5b8075e47650e997a921e592625d
Marysol Ranieri <lar_sol22@live.com>
2026-08-29 19:43:27 -0300

    feat: agregar validaciones y navegación por roles al login

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,9 +1,52 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
   selector: 'app-login',
-  styleUrl: './login.css',
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
+  styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+
+  rolSeleccionado: string = '';
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+
+    password: new FormControl('', [
+      Validators.required
+    ])
+  });
+
+  constructor(private router: Router) {}
+
+  seleccionarRol(rol: string): void {
+    this.rolSeleccionado = rol;
+  }
+
+  onSubmit(): void {
+
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    if (this.rolSeleccionado === 'cliente') {
+      this.router.navigate(['/dashboard-user']);
+    }
+
+    if (this.rolSeleccionado === 'administrador') {
+      this.router.navigate(['/dashboard-admin']);
+    }
+  }
+}
