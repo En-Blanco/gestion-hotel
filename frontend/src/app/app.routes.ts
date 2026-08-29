4b6759fc8299354f36216bcfcddf05878aa4b487
Agus Molina <agus.molina1598@gmail.com>
2026-08-29 17:29:52 -0300

    feat: adaptacion a componente de angular de registro

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,7 +4,9 @@ import { Pagina404 } from './pages/pagina-404/pagina-404';
 import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
 import { LandingPage } from './pages/landing-page/landing-page';
 import { Login } from './pages/login/login';
+import { Registro } from './pages/registro/registro';
 import { Layout } from './components/layout/layout';
+
 
 export const routes: Routes = [
   {
@@ -13,10 +15,10 @@ export const routes: Routes = [
     children: [
       { path: '', component: LandingPage },
       { path: 'dashboard-admin', component: DashboardAdmin },
-      { path: 'sobre-nosotros', component: SobreNosotros },
+      { path: 'sobre-nosotros', component: SobreNosotros }
     ],
   },
-
+  { path: 'registro', component: Registro},
   { path: 'login', component: Login },
   { path: '**', component: Pagina404 },
 ];
---
+++ frontend/src/app/pages/registro/registro.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  imports: [RouterLink],
+  selector: 'app-registro',
+  styleUrl: './registro.css',
+  templateUrl: './registro.html',
+})
+export class Registro {}
