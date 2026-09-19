3ab6545f5f911cf40f94641a06ee69596454a068
pegatica <pegatica@invap.com.ar>
2026-09-19 13:10:12 -0300

    Se agregan entidades hoteleras al json server

--- frontend/src/app/pages/dashboard-user/dashboard-user.ts
+++ frontend/src/app/pages/dashboard-user/dashboard-user.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { DashboardHeader } from '../../components/header-dashboard/header-dashboard';   // ← nueva línea
+import { DashboardHeader } from '../../components/header-dashboard/header-dashboard';
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-dashboard-user',
-  imports: [DashboardHeader],   // ← antes decía imports: []
+  imports: [DashboardHeader, Footer],
   templateUrl: './dashboard-user.html',
   styleUrl: './dashboard-user.css'
 })
