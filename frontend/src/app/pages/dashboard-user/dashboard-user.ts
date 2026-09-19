95fea593c4a44504dbcd889b4d6cd772782a08d1
BATTISTA <SE42293@grupo.ypf.com>
2026-09-19 15:32:01 -0300

    feat: se implementa flujo visual de reserva en dashboard-user

--- frontend/src/app/pages/dashboard-user/dashboard-user.ts
+++ frontend/src/app/pages/dashboard-user/dashboard-user.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DashboardHeader } from '../../components/header-dashboard/header-dashboard';
 import { Footer } from '../../components/footer/footer';
+
+import { HabitacionService } from '../../services/habitacion.service';
+import { Habitacion } from '../../models/habitacion.interface';
 
 @Component({
   selector: 'app-dashboard-user',
@@ -8,6 +11,60 @@ import { Footer } from '../../components/footer/footer';
   templateUrl: './dashboard-user.html',
   styleUrl: './dashboard-user.css'
 })
-export class DashboardUser {
+export class DashboardUser implements OnInit {
+
+  habitaciones: Habitacion[] = [];
+  mostrarHabitaciones = false;
+  mostrarServicios = false;
+  mostrarComprobante = false;
+  
+  constructor(
+    private habitacionService: HabitacionService
+  ) {}
+
+  ngOnInit(): void {
+
+    this.habitacionService.getHabitaciones().subscribe({
+      next: (data) => {
+
+        this.habitaciones = data;
+
+        console.log(
+          'Habitaciones cargadas con éxito:',
+          this.habitaciones
+        );
+
+      },
+
+      error: (err) => {
+
+        console.error(
+          'Error al cargar habitaciones:',
+          err
+        );
+
+      },
+    });
+
+  }
+
+  buscarDisponibilidad() {
+
+  // más adelante verifican reservas
+
+  this.mostrarHabitaciones = true;
+  }
+
+  confirmarHabitacion() {
+
+  this.mostrarServicios = true;
+
+  }
+
+  confirmarReserva() {
+
+  this.mostrarComprobante = true;
+
+  }
 
 }
